05370f7c85b8e5f3ac9ca29202d18e02a92f5b6f
Benjamin Kane <Benjamin.Kane@Acxiom.com>
2016-11-16 10:59:53 -0600

    Add type alias and notes

--- Factorials/main.cpp
+++ Factorials/main.cpp
@@ -3,7 +3,26 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int a);
+// This is called a 'type alias'. Now you can change the bounds of the function easily
+// just by changing this
+// using NumberType = int;
+
+// for example, to a type that can hold much larger numbers
+// on my machine (2012 Macbook Pro), this stops working at 65
+using NumberType = unsigned long long int;
+
+// To go even further, you'll need to use a custom number type:
+// http://stackoverflow.com/a/15400100/2958070
+// or a special algorithm:
+// http://stackoverflow.com/a/36898176/2958070
+
+
+// Look into the header <limits> to get the largest number holdable in each type
+// (there's good stuff in the example on this page)
+// http://en.cppreference.com/w/cpp/types/numeric_limits
+
+
+NumberType factorial(NumberType a);
 
 int main()
 {
@@ -17,11 +36,11 @@ int main()
 	return 0;
 }
 
-int factorial(int a)
+NumberType factorial(NumberType a)
 {
-	int b = 1;
-	int c = a;
-	int fact;
+	NumberType b = 1;
+	NumberType c = a;
+	NumberType fact;
 
 	for (a = a - 1; a > 0; a--)
 	{
